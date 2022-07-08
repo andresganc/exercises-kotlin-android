094d13ce4adcc6114c46b8d62e9b6856f9bcf695
Andres Giraldo <andresganc@gmail.com>
2022-07-08 11:53:24 -0500

    Mod: MainActivity.kt

--- app/src/main/java/com/nc/course_kotlin_android/MainActivity.kt
+++ app/src/main/java/com/nc/course_kotlin_android/MainActivity.kt
@@ -1,16 +1,32 @@
 package com.nc.course_kotlin_android
 
+import android.R
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.nc.course_kotlin_android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-
+        binding.btnShowName.setOnClickListener{ checkValue() }
     }
 
+    fun checkValue() {
+        if ( binding.etName.text.isNotEmpty() ) {
+            // Go to new screen
+        } else {
+            showErrorName()
+        }
+    }
 
+    fun showErrorName() {
+        Toast.makeText(this, "Add your name", Toast.LENGTH_SHORT).show()
+    }
 }
